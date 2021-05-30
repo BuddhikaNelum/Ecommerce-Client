f46cbde2c59d516b1527e746a899a14807b212ff
BudhikaN <dlbnprabath@gmail.com>
2021-05-30 14:27:02 +0530

    added shop components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagingComponent } from './components/paging/paging.component';
+
+@NgModule({
+  declarations: [
+    PagingHeaderComponent,
+    PagingComponent
+  ],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot()
+  ],
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent,
+    PagingComponent
+  ]
+})
+export class SharedModule { }
